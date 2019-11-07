f6b3c8d13d0bea0abb4eae2abf1661a18ba74db5
Jane Wang <janewangcoa@gmail.com>
2019-11-07 18:17:21 +0800

    我的静态服务器

--- node-demo-1/static-server/server.js
+++ node-demo-1/static-server/server.js
@@ -17,7 +17,6 @@ var server = http.createServer(function(request, response){
   var query = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个小可爱发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
@@ -46,7 +45,6 @@ var server = http.createServer(function(request, response){
   response.end()
   
 
-  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
