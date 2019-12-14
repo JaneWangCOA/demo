62b7254ab03bc437231b2ecce2103b8f4f8632f6
Jane Wang <janewangcoa@gmail.com>
2019-12-14 16:39:08 +0800

    把css抽取出来的模式

--- webpack-demo-1/webpack.config.js
+++ webpack-demo-1/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
@@ -9,15 +10,35 @@ module.exports = {
         path: path.resolve(__dirname,'dist'),
         filename:'[name].[contenthash].js'
     },
-    plugins:[new HtmlWebpackPlugin({
+    plugins:[
+        new HtmlWebpackPlugin({
         title: 'My App',
         template: 'assets/index.html'
-    })],
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
             },
         ],
     },
