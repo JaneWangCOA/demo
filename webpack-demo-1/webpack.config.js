37a51ed7b8b5075b2307bee40892a1a00a397f85
Jane Wang <janewangcoa@gmail.com>
2019-12-14 09:46:56 +0800

    webpack练习2

--- webpack-demo-1/webpack.config.js
+++ webpack-demo-1/webpack.config.js
@@ -1,10 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/main.js',
     output:{
         path: path.resolve(__dirname,'dist'),
         filename:'[name].[contenthash].js'
-    }
+    },
+    plugins:[new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
